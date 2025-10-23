73047110604283f42e734176a6fe1f486defb5d4
ritambrayadav <ritambrayadav20@gmail.com>
2025-10-23 21:08:31 +0530

    Intial server setup

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", 
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Socket.IO + Express server is running");
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
