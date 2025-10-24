2648b72bb33a1dd9d56bd65e1123e2d6b71d167f
ritambrayadav <ritambrayadav20@gmail.com>
2025-10-24 20:56:56 +0530

    Fix:removed cors

--- server/src/server.js
+++ server/src/server.js
@@ -11,9 +11,7 @@ import { createNewQuestion } from "./controllers/questionController.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({    
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/question", questionRoutes);
@@ -22,7 +20,7 @@ app.use("/api/user", userRoutes);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
   },
 });
 
